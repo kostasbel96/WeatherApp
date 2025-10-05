5ceb009bf96afe5b963d10915db22fa355be4625
Kostas Veloutsos <kwstasvel96@gmail.com>
2025-10-05 21:34:06 +0300

    change metric

--- src/app/shared/services/weather-api-service.ts
+++ src/app/shared/services/weather-api-service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../enviroments/enviroment';
+import { environment } from '../../../environments/environment';
 
 
 const weatherApi = `https://api.openweathermap.org/data/2.5/weather`;
@@ -12,6 +12,6 @@ export class WeatherApiService {
   http: HttpClient = inject(HttpClient);
   
   getWeatherDataByCityName(city: string): Observable<any>{
-    return this.http.get(`${weatherApi}?q=${city}&appid=${environment.weatherApiKey}&units=metrics&lang=el`);
+    return this.http.get(`${weatherApi}?q=${city}&appid=${environment.weatherApiKey}&units=metric&lang=el`);
   }
 }
